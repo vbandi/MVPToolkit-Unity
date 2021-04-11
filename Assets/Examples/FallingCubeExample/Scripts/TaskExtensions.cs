e3d3d9d5d46af26047b6f656fc1c457a379b9cdb
Velvárt András <andras.velvart@aerinx.com>
2021-04-11 17:44:28 +0200

    Moved Example project to Exmaples folder

--- Assets/Examples/FallingCubeExample/Scripts/TaskExtensions.cs
+++ Assets/Examples/FallingCubeExample/Scripts/TaskExtensions.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using UniRx;
 
 public static class TaskExtensions
 {
@@ -14,7 +13,7 @@ public static class TaskExtensions
         {
             await task;
         }
-        catch (Exception e)
+        catch (Exception)
         {
             // TODO: handle exception
         }
